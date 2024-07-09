96fcceada423364a2a413edbffd290ed60225770
TamilReactDev <levinhosmall@gmail.com>
2024-07-09 11:48:44 +0530

    'finish'

--- src/ARScene.jsx
+++ src/ARScene.jsx
@@ -6,26 +6,13 @@ import '@ar-js-org/ar.js/aframe/build/aframe-ar.js';
 import { collection, query, orderBy, startAfter, limit, getDocs, endBefore, limitToLast } from 'firebase/firestore';
 import { db } from './firebase';
 import { v4 } from 'uuid';
-
+import Loading from './Loading';
 
 
 const ARScene = () => {
 
-
-
-
-	// const initialImages = [
-	//   'https://firebasestorage.googleapis.com/v0/b/blog-e0c2d.appspot.com/o/files%2Fae76b2ac-c14b-4f83-ab82-b20699d04eb8?alt=media&token=38e3108e-bfe9-44d6-9183-5f7687c58570',
-	//   'https://images.pexels.com/photos/19734107/pexels-photo-19734107/free-photo-of-traditional-wedding-indian-couple.jpeg?auto=compress&cs=tinysrgb&w=600',
-	//   'https://images.pexels.com/photos/17261594/pexels-photo-17261594/free-photo-of-smiling-bride-and-groom-in-traditional-costumes.jpeg?auto=compress&cs=tinysrgb&w=600',
-	// ];
-
 	const [images, setImages] = useState([]);
 	const [page, setPage] = useState(1);
-
-
-
-
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
@@ -71,26 +58,26 @@ const ARScene = () => {
 
 	const showPrevious = async (item) => {
 		if (images.length === 0) {
-		  alert("No previous images.");
+			alert("No previous images.");
 		} else {
-		  try {
-			const prev = query(collection(db, 'images'), orderBy('createdAt', 'desc'), endBefore(item.createdAt), limitToLast(3));
-			const querySnapshot = await getDocs(prev);
-			const newImages = [];
-			querySnapshot.forEach((doc) => {
-			  newImages.push(doc.data());
-			});
-			if (newImages.length > 0) {
-			  setImages(newImages);
-			  setPage(page - 1);
-			} else {
-			  alert("No previous images.");
+			try {
+				const prev = query(collection(db, 'images'), orderBy('createdAt', 'desc'), endBefore(item.createdAt), limitToLast(3));
+				const querySnapshot = await getDocs(prev);
+				const newImages = [];
+				querySnapshot.forEach((doc) => {
+					newImages.push(doc.data());
+				});
+				if (newImages.length > 0) {
+					setImages(newImages);
+					setPage(page - 1);
+				} else {
+					alert("No previous images.");
+				}
+			} catch (error) {
+				console.error("Error fetching previous images: ", error);
 			}
-		  } catch (error) {
-			console.error("Error fetching previous images: ", error);
-		  }
 		}
-	  };
+	};
 
 
 
@@ -131,7 +118,7 @@ const ARScene = () => {
 
 					)
 					:
-					<div>no file found</div>
+					<Loading />
 			}
 
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,37 +1,123 @@
-import { createRoot } from 'react-dom/client'
 import { Dashboard } from './Dashboard'
 import './index.css'
 import { Link } from 'react-router-dom'
-import { useEffect } from 'react';
-
-
+import { useEffect, useState } from 'react';
+import { collection, query, orderBy, startAfter, limit, getDocs, endBefore, limitToLast } from 'firebase/firestore';
+import { db } from './firebase';
+import Loading from './Loading';
 
 function Home() {
 
+	const [images, setImages] = useState([]);
+	const [page, setPage] = useState(1);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    return () => {
-      const video = document.getElementById('arjs-video');
-      if (video) {
-        const stream = video.srcObject;
-        if (stream) {
-          const tracks = stream.getTracks();
-          tracks.forEach(track => track.stop());
-        }
-        video.srcObject = null;
-        video.parentNode.removeChild(video);
-      }
-    };
-  }, []);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				setLoading(true);
+				const first = query(collection(db, "images"), orderBy('createdAt', 'desc'), limit(10));
+				const querySnapshot = await getDocs(first);
+				const imagesData = [];
+				querySnapshot.forEach((doc) => {
+					imagesData.push(doc.data());
+				});
+				setImages(imagesData);
+				setLoading(false);
+			} catch (error) {
+				console.error("Error fetching images: ", error);
+				alert('Something went wrong. Please reload.');
+				setLoading(false);
+			}
+		};
+		fetchData();
+	}, []);
 
-  return (
-    <>
+	const showNext = async (item) => {
+		if (images.length === 0) {
+			alert("That's all we have for now!");
+		} else {
+			try {
+				setLoading(true);
+				const next = query(collection(db, 'images'), orderBy('createdAt', 'desc'), startAfter(item.createdAt), limit(10));
+				const querySnapshot = await getDocs(next);
+				const newImages = [];
+				querySnapshot.forEach((doc) => {
+					newImages.push(doc.data());
+				});
+				if (newImages.length > 0) {
+					setImages(newImages);
+					setPage(page + 1);
+				} else {
+					alert("No more images.");
+				}
+				setLoading(false);
+			} catch (error) {
+				console.error("Error fetching next images: ", error);
+				setLoading(false);
+			}
+		}
+	};
 
-     <Link style={{position:'absolute',zIndex:"1000", top:0, left:0,  color:'red'}} to={'/ar'}>enter vr mode</Link>
-      <Dashboard />
-    </>
-  )
+	const showPrevious = async (item) => {
+		if (images.length === 0) {
+			alert("No previous images.");
+		} else {
+			try {
+				setLoading(true);
+				const prev = query(collection(db, 'images'), orderBy('createdAt', 'desc'), endBefore(item.createdAt), limitToLast(10));
+				const querySnapshot = await getDocs(prev);
+				const newImages = [];
+				querySnapshot.forEach((doc) => {
+					newImages.push(doc.data());
+				});
+				if (newImages.length > 0) {
+					setImages(newImages);
+					setPage(page - 1);
+				} else {
+					alert("No previous images.");
+				}
+				setLoading(false);
+			} catch (error) {
+				console.error("Error fetching previous images: ", error);
+				setLoading(false);
+			}
+		}
+	};
+
+	useEffect(() => {
+		return () => {
+			const video = document.getElementById('arjs-video');
+			if (video) {
+				const stream = video.srcObject;
+				if (stream) {
+					const tracks = stream.getTracks();
+					tracks.forEach(track => track.stop());
+				}
+				video.srcObject = null;
+				video.parentNode.removeChild(video);
+			}
+		};
+	}, []);
+
+	return (
+		<>
+			<Link style={{ position: 'absolute', zIndex: '1000', top: 10, left: 10, padding: '10px 20px', backgroundColor: 'lightgray', color: 'red', textDecoration: 'none', borderRadius: '5px' }} to={'/ar'}>
+				Enter AR Mode
+			</Link>
+			<button onClick={() => showPrevious(images[0])} style={{ position: 'absolute', zIndex: '1000', bottom: 10, left: 10, padding: '10px 20px', backgroundColor: 'lightgray', color: 'red', textDecoration: 'none', borderRadius: '5px' }}>
+				Prev
+			</button>
+			<button onClick={() => showNext(images[images.length - 1])} style={{ position: 'absolute', zIndex: '1000', bottom: 10, right: 10, padding: '10px 20px', backgroundColor: 'lightgray', color: 'red', textDecoration: 'none', borderRadius: '5px' }}>
+				Next
+			</button>
+			{loading ? (
+				<Loading />
+			) : (
+				<Dashboard images={images} page={page} />
+			)}
+		</>
+	)
 }
-
 
 export default Home
